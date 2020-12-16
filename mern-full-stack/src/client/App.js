eabde8d1534feb9c59d92b5f3ff0b85e9c04f8d4
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-12-16 22:28:28 +0100

    Packages and Files

--- mern-full-stack/src/client/App.js
+++ mern-full-stack/src/client/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 //import required components
-import CreateUser from './CreateUser';
-import EditUser from './EditUser';
-import UserList from './UserList';
+import CreateBook from './CreateBook';
+import EditBook from './EditBook';
+import BookList from './BookList';
 
 // this is the "main" component which sets up the React Router and respective routes
 const App = () => {
@@ -11,11 +11,11 @@ const App = () => {
     <HashRouter>
       <div>
         {/*SERVERSIDE: Link the routes to components*/}
-        <Route exact path="/" component={UserList}/>
-        {/*pass the id through the EditUser component*/}
-        <Route path="/edit-user/:id" component={EditUser}/>
-        {/*set the path to create a new user to CreateUser component*/}
-        <Route path="/create-user" component={CreateUser}/>
+        <Route exact path="/" component={BookList}/>
+        {/*pass the id through the EditBook component*/}
+        <Route path="/edit-book/:id" component={EditBook}/>
+        {/*set the path to create a new user to CreateBook component*/}
+        <Route path="/create-book" component={CreateBook}/>
       </div>
     </HashRouter>
   );
